9e2cde7a2a7bc6312d553c2488146272b4b51a86
Christian Zaldivar <zh15002@ues.edu.sv>
2019-06-04 00:52:46 -0600

    adding first resource

---
+++ src/main/java/sv/edu/uesocc/tpi2019/tester/boundary/TestResource.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.tpi2019.tester.boundary;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author zaldivar
+ */
+@Path("/test")        
+public class TestResource {
+    
+ private final List<String> model=List.of("A", "B", "C", "D", "F");
+    
+ @GET
+ @Produces(MediaType.APPLICATION_JSON+";charset=utf-8")
+ public List<String> findall(){
+     return this.model;
+ } 
+ 
+}
